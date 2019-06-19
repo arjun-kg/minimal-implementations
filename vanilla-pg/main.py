f2efd5a1e1ae26648890362e7a19085ea44adc6f
arjun-kg <kg.arjun@gmail.com>
2019-06-19 17:18:19 +0530

    vpg working for cartpole-v1

--- vanilla-pg/main.py
+++ vanilla-pg/main.py
@@ -1,97 +1,93 @@
 import torch
 import torch.nn as nn
-import gym 
-import pdb 
-import numpy as np 
-import torch.optim as optim 
+import gym
+import pdb
+import numpy as np
+import torch.optim as optim
+from tensorboardX import SummaryWriter
+from datetime import datetime
 
-
-n_episodes = 10000
-hidden_size = 256
+n_episodes = 5000
+hidden_size = 6
 train_flag = True
 gamma = 0.99
-lr = 1e-4
-eps_start = 0.8
-eps_end = 0.2
+eps_start = 0
+eps_end = 0.0
 temperature = n_episodes/10
+seed = 0
 
-env = gym.make('Pendulum-v0')
+env = gym.make('CartPole-v1')
 input_size = env.observation_space.shape[0]
-output_size = env.action_space.shape[0]
-action_scale = env.action_space.high
-learn_std = False
-eval_every = 20
+output_size = env.action_space.n
+eval_every = 25
+np.random.seed(seed)
+env.seed(seed)
+torch.manual_seed(seed)
 
+w1 = nn.Parameter(torch.zeros(input_size, hidden_size))
+nn.init.xavier_uniform_(w1)
+b1 = nn.Parameter(torch.zeros(1, hidden_size))
+w2 = nn.Parameter(torch.zeros(hidden_size, output_size))
+nn.init.xavier_uniform_(w2)
+b2 = nn.Parameter(torch.zeros(1, output_size))
 
-#policy = nn.Sequential(nn.Linear(input_size, hidden_size), 
-#                                         nn.ReLU(), 
-#                                         nn.Linear(hidden_size, output_size), 
-#                                         nn.Tanh())
-
-w1 = nn.Parameter(torch.randn(input_size, hidden_size))
-b1 = nn.Parameter(0.01*torch.ones(1, hidden_size))
-w2 = nn.Parameter(torch.randn(hidden_size, output_size))
-b2 = nn.Parameter(0.01*torch.ones(1, output_size))
-
-#optimizer = optim.Adam(policy.parameters())
-
-log_action_std = nn.Parameter(-2*torch.ones(output_size))
+trainable_params = [w1, w2, b1, b2]
+optimizer = optim.Adam(trainable_params)
+writer = SummaryWriter('/tmp/rl_implementations/vpg/{}'.format(datetime.today().strftime("%Y-%d-%b-%H-%M-%S")))
 
 for ep in range(n_episodes):
     done = False
     state = env.reset()
-    
     states = [state]
     action_log_probs = []
     rews = []
     eps_now = eps_end + (eps_start - eps_end)*np.exp(-ep/temperature)
-    while not done:
-        x = torch.relu(torch.mm(torch.tensor(state).view(-1,input_size).float(), w1) + b1)
-        x = torch.tensor(action_scale)*torch.tanh(torch.mm(x, w2) + b2)
-        action_mean = x               
-        action_std = torch.exp(log_action_std)
-        action = torch.normal(action_mean,action_std)
 
+    while not done:
+        x = nn.ELU()(torch.mm(torch.tensor(state).view(-1,input_size).float(), w1) + b1)
+        out = torch.tanh(torch.mm(x, w2) + b2)
+
+        action_prob = torch.softmax(out, dim=-1)
+        action_log_prob = torch.log_softmax(out, dim=-1)  # taking log of action_prob is numerically unstable
+        # action = torch.multinomial(action_prob, 1).item()
+        action = action_prob.max(-1)[1].item()
         if eps_now > np.random.rand():
-            action = action_scale*(2*torch.rand(output_size) - 1)
-        log_prob = torch.distributions.Normal(action_mean, action_std).log_prob(action)
-        state, rew, done, _ = env.step(action.detach())
-        
+            action = np.random.randint(output_size)
+        state, rew, done, _ = env.step(action)
+
         rews.append(rew)
         states.append(state)
-        action_log_probs.append(log_prob)
+        selected_action_log_prob = torch.index_select(action_log_prob, dim=-1, index=torch.tensor(action)).view(1, 1)
+        action_log_probs.append(selected_action_log_prob)
 
     if train_flag:
         returns = [rews[-1]]
         for rew in reversed(rews[:-1]):
-            returns.append(rew.float() + gamma*returns[-1].float())
-        
+            returns.append(rew + gamma*returns[-1])
         returns.reverse()
-        
-        loss = torch.mean(torch.stack(action_log_probs)*torch.tensor(returns).float())
+        # policy loss
+        loss = -torch.mean(torch.stack(action_log_probs).squeeze(1).squeeze(1)*torch.tensor(returns).float())
         loss.backward()
-        
-        for par in [w1, b1, w2, b2]:
-            par.data.add_(lr*par.grad.data)
-            par.grad.data.zero_()
-        if learn_std:
-            log_action_std.data.add_(lr*log_action_std.grad.data)
-            log_action_std.grad.data.zero_()
-        
+        optimizer.step()
+        optimizer.zero_grad()
+
+    writer.add_scalar('train/Epsilon', eps_now, ep)
+    writer.add_scalar('train/Loss', loss, ep)
+
     if ep % eval_every == 0:
         state = env.reset()
         done = False
-        action_std = torch.exp(log_action_std)            
         eval_rews = 0
         while not done:
-            env.render()
-            x = torch.relu(torch.mm(torch.tensor(state).view(-1,input_size).float(), w1) + b1)
-            x = torch.tensor(action_scale)*torch.tanh(torch.mm(x, w2) + b2)
-            action_mean = x
-            action = torch.normal(action_mean,action_std)
-            state, rew, done, _ = env.step(action.detach())
+            # env.render()
+            x = nn.ELU()(torch.mm(torch.tensor(state).view(-1, input_size).float(), w1) + b1)
+            out = torch.tanh(torch.mm(x, w2) + b2)
+
+            # action_probs = torch.softmax(out, dim=-1)
+            # action = torch.multinomial(action_probs, 1).item()
+            action = out.max(-1)[1].item()
+            state, rew, done, _ = env.step(action)
             eval_rews += rew
-        
+
         print("Episodes: {}, Eval rews: {}, Eps now: {}".format(ep, eval_rews, round(eps_now,2)))
-            
-        
+        writer.add_scalar('eval/rewards', eval_rews, ep)
